031995625516e7001c18092dd65b5003e530be92
DianaLeb <91115936+DianaLeb@users.noreply.github.com>
2021-09-21 10:37:49 +0300

    Add files via upload
    1

---
+++ Projct 1/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	package com.company;
+import java.util.Scanner;
+        public class Main {
+
+            public static void main(String[] args) {
+                nt x = 20;
+                if (x > 22) {
+                    if (x < 22) {
+                        System.out.println("it works");
+                    } else {
+                        System.out.println("it doesn't work");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 2;
+        int y =5;
+        if(x>y)
+            System.out.println(x);
+else
+            System.out.println(y);
+        int x = 2;
+        int y =5;
+        if(x>y){
+            System.out.println(x);
+        }
+        else{
+            System.out.println(y);
+        }
